8913d10059504848d4c0398e4b206f5dd2913d85
Alexander A. Klimov <grandmaster@al2klimov.de>
2023-01-14 12:51:00 +0100

    unveil(2)

--- server/hardening_openbsd.go
+++ server/hardening_openbsd.go
@@ -6,6 +6,22 @@ package main
 import "golang.org/x/sys/unix"
 
 func hardening() {
+	if err := unix.Unveil(".", "r"); err != nil {
+		panic(err)
+	}
+
+	if err := unix.Unveil("/usr", "rx"); err != nil {
+		panic(err)
+	}
+
+	if err := unix.Unveil("/dev/ptm", "rw"); err != nil {
+		panic(err)
+	}
+
+	if err := unix.UnveilBlock(); err != nil {
+		panic(err)
+	}
+
 	if err := unix.PledgePromises("stdio rpath wpath inet tty proc exec"); err != nil {
 		panic(err)
 	}
